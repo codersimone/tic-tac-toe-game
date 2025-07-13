575740e80831599caa182e9743e7417c34c88e2e
codersimone <97359905+codersimone@users.noreply.github.com>
2025-07-14 01:46:11 +0300

    Created the Gameboard object as an IIFE module.

---
+++ script/script.js
@@ -0,0 +1,25 @@
+const gameboard = (function () {
+    let board = ['', '', '', '', '', '', '', '', ''];
+
+    function getBoard() {
+        return board;
+    }
+
+    function setCell(index, cell) {
+        if (board[index] === '') {
+            board[index] = cell;
+            return true;
+        }
+        return false;
+    }
+
+    function resetBoard() {
+        board = ['', '', '', '', '', '', '', '', ''];
+    }
+
+    return {
+        getBoard,
+        setCell,
+        resetBoard,
+    };
+})();
