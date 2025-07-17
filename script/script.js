39b3c5fd73086e026dd850a49c09970b4d63577c
codersimone <97359905+codersimone@users.noreply.github.com>
2025-07-17 18:26:40 +0300

    Created the GameController object as an IIFE module responsible for the game logic via the console.

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-const gameboard = (function () {
+const gameBoard = (function () {
     let board = ['', '', '', '', '', '', '', '', ''];
 
     function getBoard() {
@@ -30,3 +30,94 @@ function Player(name, symbol) {
         getSymbol: () => symbol,
     };
 }
+
+const gameController = (function () {
+    let firstPlayer;
+    let secondPlayer;
+    let currentPlayer;
+    let gameOver = false;
+
+    function setPlayers(firstPlayerName, secondPlayerName) {
+        firstPlayer = Player(firstPlayerName, 'X');
+        secondPlayer = Player(secondPlayerName, 'O');
+        currentPlayer = firstPlayer;
+    }
+
+    function getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    function switchPlayer() {
+        currentPlayer =
+            currentPlayer === firstPlayer ? secondPlayer : firstPlayer;
+    }
+
+    function playRound(index) {
+        const board = gameBoard.getBoard();
+
+        if (gameOver || !gameBoard.setCell(index, currentPlayer.getSymbol())) {
+            return;
+        }
+
+        if (checkWinner(board)) {
+            console.log(`${currentPlayer.getName()} wins!`);
+            gameOver = true;
+        } else if (checkDraw(board)) {
+            console.log(`It's a draw!`);
+            gameOver = true;
+        } else {
+            switchPlayer();
+        }
+    }
+
+    function checkWinner(board) {
+        const winningRules = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for (let i = 0; i < winningRules.length; i++) {
+            const [a, b, c] = winningRules[i];
+            if (
+                board[a] !== '' &&
+                board[a] === board[b] &&
+                board[a] === board[c]
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function checkDraw(board) {
+        if (checkWinner(board)) {
+            return false;
+        }
+
+        for (let i = 0; i < board.length; i++) {
+            if (board[i] === '') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function restart() {
+        gameBoard.resetBoard();
+        currentPlayer = firstPlayer;
+        gameOver = false;
+    }
+
+    return {
+        setPlayers,
+        getCurrentPlayer,
+        playRound,
+        restart,
+    };
+})();
